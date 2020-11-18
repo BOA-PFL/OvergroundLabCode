70d116066f826b7e757cba64f78a47af2ded38b4
Dan Feeney <daniel.feeney@colorado.edu>
2020-11-18 09:37:06 -0700

    changing variable names to match new export

--- Python/SingleLegLanding.py
+++ Python/SingleLegLanding.py
@@ -74,16 +74,16 @@ ankleSagMom = []
 ankleFrontMom = []
 kneeSagMom = []
 kneeFrontMom = []
-hipSagMom = []
-hipFrontMom = []
-ankleFrontAng = []
-ankleSagAng = []
-ankleTransAng = []
-kneeFrontAng = []
-kneeTransAng = []
-hipFrontAng = []
-hipSagAng = [] 
-hipTransAng = []
+#hipSagMom = []
+#hipFrontMom = []
+ankleXAng = []
+ankleYAng = []
+kneeYAng = []
+kneeZAng = []
+kneeXAng = []
+#hipFrontAng = []
+#hipSagAng = [] 
+#hipTransAng = []
 ## loop through the selected files
 for file in entries:
     try:
@@ -96,7 +96,7 @@ for file in entries:
         config = fName.split(sep = "_")[2]
         
         # Filter force
-        forceZ = dat.FP4Z * -1
+        forceZ = dat.Column22 * -1
         forceZ[forceZ<fThresh] = 0
         #find the landings and offs of the FP as vectors
         landings = findLandings(forceZ)
@@ -113,25 +113,25 @@ for file in entries:
                 stabilization.append(findStabilization(avgF, sdF))
                 tmpStab = findStabilization(avgF, sdF)
                 ## Work ##
-                ankleWork.append(sum(abs(dat.RAnklePower[landing : landing + tmpStab])))
-                kneeWork.append(sum(abs(dat.RKneePower[landing : landing + tmpStab])))
-                hipWork.append(sum(abs(dat.RHipPower[landing : landing + tmpStab])))
+                ankleWork.append(sum(abs(dat.LAnklePower[landing : landing + tmpStab])))
+                kneeWork.append(sum(abs(dat.LKneePower[landing : landing + tmpStab])))
+                #hipWork.append(sum(abs(dat.LHipPower[landing : landing + tmpStab])))
                 ## Pk Moments ##
-                ankleSagMom.append(np.max(dat.RightAnkleMomentSagittal[landing : landing + tmpStab]))
-                ankleFrontMom.append(np.max(dat.RightAnkleMomentFrontal[landing : landing + tmpStab]))
-                kneeSagMom.append(np.min(dat.RightKneeMomentSagittal[landing : landing + tmpStab]))
-                kneeFrontMom.append(np.min(dat.RightKneeMomentFrontal[landing : landing + tmpStab]))
-                hipSagMom.append(np.max(dat.RightHipMomentSagittal[landing : landing + tmpStab]))
-                hipFrontMom.append(np.max(dat.RightHipMomentFrontal[landing : landing + tmpStab]))
+                ankleSagMom.append(np.max(dat.LAnkleMomentx[landing : landing + tmpStab]))
+                ankleFrontMom.append(np.max(dat.LAnkleMomenty[landing : landing + tmpStab]))
+                kneeSagMom.append(np.min(dat.LKneeMomentX[landing : landing + tmpStab]))
+                kneeFrontMom.append(np.min(dat.LKneeMomentY[landing : landing + tmpStab]))
+                #hipSagMom.append(np.max(dat.LeftHipMomentSagittal[landing : landing + tmpStab]))
+                #hipFrontMom.append(np.max(dat.LeftHipMomentFrontal[landing : landing + tmpStab]))
                 ## Angles ## 
-                ankleFrontAng.append(np.max(dat.RightAnkleAngleFrontal[landing : landing + tmpStab]))
-                ankleSagAng.append(np.min(dat.RightAnkleAngleSagittal[landing : landing + tmpStab]))
-                ankleTransAng.append(np.min(dat.RightAnkleAngleTransverse[landing : landing + tmpStab]))
-                kneeFrontAng.append(np.max(dat.RightAnkleAngleFrontal[landing : landing + tmpStab]))
-                kneeTransAng.append(np.max(dat.RightKneeAngleTransverse[landing : landing + tmpStab]))
-                hipFrontAng.append(np.max(dat.RightHipAngleFrontal[landing : landing + tmpStab]))
-                hipSagAng.append(np.min(dat.RightHipAngleSagittal[landing : landing + tmpStab]))
-                hipTransAng.append(np.min(dat.RightHipAngleTransverse[landing : landing + tmpStab]))
+                ankleXAng.append(np.max(dat.LAnkleAngleX[landing : landing + tmpStab]))
+                ankleYAng.append(np.min(dat.LAnkleAngleY[landing : landing + tmpStab]))
+                kneeYAng.append(np.max(dat.LKneeYAngle[landing : landing + tmpStab]))
+                kneeZAng.append(np.max(dat.LKneeZAngle[landing : landing + tmpStab]))
+                kneeXAng.append(np.max(dat.LKneeXAngle[landing : landing + tmpStab]))
+                #hipFrontAng.append(np.max(dat.LeftHipAngleFrontal[landing : landing + tmpStab]))
+                #hipSagAng.append(np.min(dat.LeftHipAngleSagittal[landing : landing + tmpStab]))
+                #hipTransAng.append(np.min(dat.LeftHipAngleTransverse[landing : landing + tmpStab]))
                 
                 
             except:
@@ -141,11 +141,12 @@ for file in entries:
             print(file)
 
 outcomes = pd.DataFrame({'Sub':list(sName), 'Config': list(tmpConfig), 'StabTime': list(stabilization),
-                         'ankleWork': list(ankleWork), 'kneeWork': list(kneeWork),'hipWork': list(hipWork),
+                         'ankleWork': list(ankleWork), 'kneeWork': list(kneeWork),
                          'ankleSagMom':list(ankleSagMom),'ankleFrontMom': list(ankleFrontMom), 'kneeSagMom':list(kneeSagMom),
-                         'kneeFrontMom':list(kneeFrontMom), 'hipSagMom':list(hipSagMom), 'ankleFrontAng':list(ankleFrontAng),
-                         'ankleSagAng':list(ankleSagAng),'kneeFrontAng':list(kneeFrontAng),'kneeTransAng':list(kneeTransAng),
-                         'hipFrontAng':list(hipFrontAng), 'hipSagAng':list(hipSagAng),'hipTransAng':list(hipTransAng)})
+                         'kneeFrontMom':list(kneeFrontMom),'ankleYAng':list(ankleYAng),
+                         'ankleXAng':list(ankleXAng),'kneeXAng':list(kneeXAng),'kneeYAng':list(kneeYAng), 'kneeZAng':list(kneeZAng)})#,
+                         #'hipFrontAng':list(hipFrontAng), 'hipSagAng':list(hipSagAng),'hipTransAng':list(hipTransAng), 
+                         #,'hipWork': list(hipWork), 'hipSagMom':list(hipSagMom),})
     
     
 # List comprehension is written below & probably faster but not necessary for this code #
