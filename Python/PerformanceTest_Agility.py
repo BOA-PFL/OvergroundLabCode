aaa69b134e38078173370a2408b374e18e062438
Kate Harrison <55414964+kharrison89@users.noreply.github.com>
2022-05-19 13:21:56 -0600

    Update PerformanceTest_Agility.py
    correcting units and column names for new TMM export

--- Python/PerformanceTest_Agility.py
+++ Python/PerformanceTest_Agility.py
@@ -14,7 +14,7 @@ import os
 fThresh = 80 #below this value will be set to 0.
 
 # Read in balance file
-fPath = 'C:/Users/kate.harrison/Boa Technology Inc/PFL - Documents/General/AgilityPerformanceData/BOA_DualPanel_Sept2021/Overground/'
+fPath = 'C:/Users/kate.harrison/Boa Technology Inc/PFL Team - Documents/General/Testing Segments/AgilityPerformanceData/DialLocation_CPDMech_April2022 (NOBULL)/Overground/'
 fileExt = r".txt"
 entries = [fName for fName in os.listdir(fPath) if fName.endswith(fileExt)]
 
@@ -62,6 +62,7 @@ def delimitTrialCMJ(inputDF):
 
 CT = []
 impulse = []
+jumpTime = []
 
 subName = []
 config = []
@@ -72,10 +73,10 @@ movements = []
 for fName in entries:
     try:
         
-        #fName = entries[0]
+        #fName = entries[1]
         
-        config1 = fName.split('_')[2]
-        tmpMove = fName.split('_')[3].split(' ')[0]
+        config1 = fName.split('_')[1]
+        tmpMove = fName.split('_')[2]
         
         dat = pd.read_csv(fPath+fName,sep='\t', skiprows = 8, header = 0)
         #dat = dat.fillna(0)
@@ -83,34 +84,38 @@ for fName in entries:
         if (tmpMove == 'Skater') or (tmpMove == 'skater'):
             
             # create vector of force from vertical signal from each file and make low values 0
-            if np.max(abs(dat.FP3_Z)) > np.max(abs(dat.FP4_Z)):
-                ZForce = dat.FP3_Z *-1
-                XForce = dat.FP3_Y
+            if np.max(abs(dat.FP3_GRF_Z)) > np.max(abs(dat.FP4_GRF_Z)):
+                ZForce = dat.FP3_GRF_Z
+                XForce = dat.FP3_GRF_X
                 
             else:
-                ZForce = dat.FP4_Z * -1
-                XForce = dat.FP4_Y 
+                ZForce = dat.FP4_GRF_Z
+                XForce = dat.FP4_GRF_X 
                 
             if abs(np.min(XForce)) > abs(np.max(XForce)):
                 XForce = XForce * -1
-                         
+            
+            #dat = delimitTrialSkate(dat)
             ZForce[ZForce<fThresh] = 0
             
-            dat = delimitTrialSkate(dat)
+            
             #find the landings from function above
             landings = findLandings(ZForce)
             takeoffs = findTakeoffs(ZForce)
-        
+            
+            landings[:] = [x for x in landings if x < takeoffs[-1]]
+            takeoffs[:] = [x for x in takeoffs if x > landings[0]]
+            
         elif (tmpMove == 'CMJ') or (tmpMove == 'cmj'):
             
             
             
-            ZForce = dat.FP2_Z * -1
+            ZForce = dat.FP2_GRF_Z
             ZForce[ZForce<fThresh] = 0
             
             XForce = ZForce #This is out of convenience to calculate impulse below even though this is not the Y force
             
-            dat = delimitTrialCMJ(dat)
+            #dat = delimitTrialCMJ(dat)
             
             landings = findLandings(ZForce)
             takeoffs = findTakeoffs(ZForce)
@@ -122,12 +127,13 @@ for fName in entries:
             print('this movement is not included in Performance Test Analysis')
         
         
-        for countVar, landing in enumerate(landings):
+        for i in range(len(landings) - 1):
             try:
                 
-                
-                CT.append(takeoffs[countVar] - landing)
-                impulse.append(np.sum(XForce[landing:takeoffs[countVar]]) )
+                impulse.append(np.sum(XForce[landings[i]:takeoffs[i]]) )
+                CT.append(takeoffs[i] - landings[i])
+                jumpTime.append((landings[i+1] - landings[i])/100)
+            
 
                 subName.append(fName.split('_')[0])
                 config.append( config1 )
@@ -135,13 +141,13 @@ for fName in entries:
                 
                 
             except:
-                print(landing)
+                print(fName + str(i))
     except:
         print(fName)
 
 
 outcomes = pd.DataFrame({'Subject':list(subName), 'Config': list(config), 'Movement':list(movements),
-                         'CT':list(CT), 'impulse':list(impulse) })
+                         'CT':list(CT), 'impulse':list(impulse), 'jumpTime':list(jumpTime) })
 
 outfileName = fPath + 'CompiledAgilityData.csv'
 outcomes.to_csv(outfileName, index = False)
