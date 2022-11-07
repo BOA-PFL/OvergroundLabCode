43a5594981703d400f31c275e881ce07b2e84e50
Dan Feeney <dan.feeney31@gmail.com>
2022-11-07 16:11:04 -0700

    changing plot vars

--- Python/PerformanceTest_Agility.py
+++ Python/PerformanceTest_Agility.py
@@ -144,36 +144,41 @@ def makeVizPlot(inputDF, inputLandings, inputTakeoffs):
     No variables; just a plot to inspect for clean kinematic/kinetic data.
 
     """
-    fig, (ax, ax1, ax2) = plt.subplots(1,3)
+    fig, (ax, ax1) = plt.subplots(1,2)
     ax.plot(inputDF.RAnkleAngle_Sagittal[inputLandings[0]:inputTakeoffs[3]], label = 'Right Sagittal')
     ax.plot(inputDF.LAnkleAngle_Sagittal[inputLandings[0]:inputTakeoffs[3]], label = 'Left Sagittal')
     ax.plot(inputDF.RAnkleAngle_Frontal[inputLandings[0]:inputTakeoffs[3]], label = 'Right Frontal')
     ax.plot(inputDF.LAnkleAngle_Frontal[inputLandings[0]:inputTakeoffs[3]], label = 'Left Frontal')
+    ax.vlines(x = inputLandings[0:3], ymin = 0, ymax = np.max(inputDF.RAnkleAngle_Sagittal[inputLandings[0:3]]),
+       color = 'red', label = 'Landings',linewidth=3.0, ls='--')
+    ax.vlines(x = inputTakeoffs[0:3], ymin = 0, ymax = np.max(inputDF.RAnkleAngle_Sagittal[inputLandings[0:3]]),
+       color = 'green', label = 'Takeoffs',linewidth=3.0, ls='--')
     ax.set_xlabel('Indices')
     ax.set_title('Ankle')
     ax.set_ylabel('Angle (Deg)')
     ax.legend()
-    ax1.plot(inputDF.RKneeAngle_Sagittal[inputLandings[0]:inputTakeoffs[3]], label = 'Right Sagittal')
-    ax1.plot(inputDF.LKneeAngle_Sagittal[inputLandings[0]:inputTakeoffs[3]], label = 'Left Sagittal')
     ax1.plot(inputDF.RKneeAngle_Frontal[inputLandings[0]:inputTakeoffs[3]], label = 'Right Frontal')
     ax1.plot(inputDF.LKneeAngle_Frontal[inputLandings[0]:inputTakeoffs[3]], label = 'Left Frontal')
-    ax1.set_xlabel('Indices')
+    ax1.vlines(x = inputLandings[0:3], ymin = 0, ymax = np.max(inputDF.RKneeAngle_Frontal[inputLandings[0:3]]),
+       color = 'red', label = 'Landings',linewidth=3.0, ls='--')
+    ax1.vlines(x = inputTakeoffs[0:3], ymin = 0, ymax = np.max(inputDF.RKneeAngle_Frontal[inputLandings[0:3]]),
+       color = 'green', label = 'Takeoffs',linewidth=3.0, ls='--')
+    ax1.set_xlabel('Indices') 
     ax1.set_title('Knee')
     ax1.set_ylabel('Angle (Deg)')
-    ax2.plot(inputDF.RHipAngle_Sagittal[inputLandings[0]:inputTakeoffs[3]], label = 'Right Sagittal')
-    ax2.plot(inputDF.LHipAngle_Sagittal[inputLandings[0]:inputTakeoffs[3]], label = 'Left Sagittal')
-    ax2.plot(inputDF.RHipAngle_Frontal[inputLandings[0]:inputTakeoffs[3]], label = 'Right Frontal')
-    ax2.plot(inputDF.LHipAngle_Frontal[inputLandings[0]:inputTakeoffs[3]], label = 'Left Frontal')
-    ax2.set_xlabel('Indices')
-    ax2.set_title('Hip')
-    ax2.set_ylabel('Angle (Deg)')
     plt.tight_layout()
     ax1.legend()
     plt.show()
     
-    fig2, (ax, ax1, ax2) = plt.subplots(1,3)
+    fig2, (ax, ax1) = plt.subplots(1,2)
     ax.plot(inputDF.RAnkleMoment_Sagittal[inputLandings[0]:inputTakeoffs[3]], label = 'Right Sagittal')
     ax.plot(inputDF.LAnkleMoment_Sagittal[inputLandings[0]:inputTakeoffs[3]], label = 'Left Sagittal')
+    ax.plot(inputDF.RAnkleMoment_Frontal[inputLandings[0]:inputTakeoffs[3]], label = 'Right Frontal ')
+    ax.plot(inputDF.LAnkleMoment_Frontal[inputLandings[0]:inputTakeoffs[3]], label = 'Left Frontal ')
+    ax.vlines(x = inputLandings[0:3], ymin = 0, ymax = np.max(inputDF.RAnkleMoment_Sagittal[inputLandings[0:3]]),
+       color = 'red', label = 'Landings',linewidth=3.0, ls='--')
+    ax.vlines(x = inputTakeoffs[0:3], ymin = 0, ymax = np.max(inputDF.RAnkleMoment_Sagittal[inputLandings[0:3]]),
+       color = 'green', label = 'Takeoffs',linewidth=3.0, ls='--')
     ax.set_xlabel('Indices')
     ax.set_title('Ankle')
     ax.set_ylabel('Moment (Nm)')
@@ -183,14 +188,19 @@ def makeVizPlot(inputDF, inputLandings, inputTakeoffs):
     ax1.set_xlabel('Indices')
     ax1.set_title('Knee')
     ax1.set_ylabel('Moment (Nm)')
-    ax2.plot(inputDF.RHipMoment_Sagittal[inputLandings[0]:inputTakeoffs[3]], label = 'Right Sagittal')
-    ax2.plot(inputDF.LHipMoment_Sagittal[inputLandings[0]:inputTakeoffs[3]], label = 'Left Sagittal')
-    ax2.set_xlabel('Indices')
-    ax2.set_title('Hip')
-    ax2.set_ylabel('Moment (Nm)')
     plt.tight_layout()
     ax1.legend()
+    ax1.vlines(x = inputLandings[0:3], ymin = 0, ymax = np.max(inputDF.RKneeMoment_Sagittal[inputLandings[0:3]]),
+       color = 'red', label = 'Landings',linewidth=3.0, ls='--')
+    ax1.vlines(x = inputTakeoffs[0:3], ymin = 0, ymax = np.max(inputDF.RKneeMoment_Sagittal[inputLandings[0:3]]),
+       color = 'green', label = 'Takeoffs',linewidth=3.0, ls='--')
     plt.show()
+    
+    # fig3, ax = plt.subplots(1,1)
+    # ax.plot(dat.COM_Power)
+    # ax.set_ylabel('Power (W)')
+    # ax.set_title('COM Power')
+    # plt.show()
 
 
 CT = []
