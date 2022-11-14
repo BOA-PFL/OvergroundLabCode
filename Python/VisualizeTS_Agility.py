5cc52e1e35ef5ef54fa53e5a1821c4985a1a5244
Dan Feeney <dan.feeney31@gmail.com>
2022-11-14 14:38:17 -0700

    pushing vis plot

--- Python/VisualizeTS_Agility.py
+++ Python/VisualizeTS_Agility.py
@@ -1,29 +1,36 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon May 17 14:48:59 2021
+Created on Mon Nov 14 13:36:58 2022
 
-Updating 11/7/22 to include data visualization plots to 'pass' or fail
-and append the failing file names to a list while the passing ones write
-results to a csv - DF
+Visualizing time series plots of common varibales for agility data
 
+@author: Dan.Feeney
 """
+
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import signal
 import os
-import copy
 from tkinter import messagebox
+from dataclasses import dataclass
 
-pd.options.mode.chained_assignment = None  # default='warn' set to warn for a lot of warnings
+### main inputs ###
 
-# Define constants and options
-fThresh = 80 #below this force value will be set to 0.
-save_on = 0 # turn this on for automatic saving of csv!!!! 
+# select files
+
 fPath = 'C:\\Users\\daniel.feeney\\Boa Technology Inc\\PFL Team - General\\Testing Segments\AgilityPerformanceData\\CPD_TongueLocatedDial_Oct2022\\Overground\\'
-
 fileExt = r".txt"
 entries = [fName for fName in os.listdir(fPath) if fName.endswith(fileExt)]
+entries = os.listdir(fPath)
+
+# Choose two files to compare
+fName1 = entries[1]
+fName2 = entries[2]
+fThresh = 40
+
+stepLen = 100
 
 ### set plot font size ###
 SMALL_SIZE = 14
@@ -37,6 +44,55 @@ plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
 plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
 plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
 plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+pd.options.mode.chained_assignment = None  # default='warn' set to warn for a lot of warnings
+
+#############################################################################
+# Functions and classes #
+# instantiate a dataclass to be used below
+
+@dataclass
+class JumpStarts:
+    movement: str
+    config: str
+    landings: list
+    takeoffs: list
+    df: pd.DataFrame
+
+@dataclass
+class EnsembleData:
+    meanData: np.ndarray
+    sdData: np.ndarray
+    
+    
+# preallocate matrix for force and fill in with force data
+def forceMatrix(inputForce, landings, noSteps, stepLength):
+    """
+    input a force signal, return matrix with n rows (for each landing) by m col
+    #for each point in stepLen
+    """
+    preForce = np.zeros((noSteps,stepLength))
+    
+    for iterVar, landing in enumerate(landings):
+        try:
+            preForce[iterVar,] = inputForce[landing:landing+stepLength]
+        except:
+            print(landing)
+            
+    return preForce
+
+
+def calcEnsembleData(Force, TurnStarts, stepLength):
+    """ 
+    Calculates ensemble averaged force and SD data from a time series
+    """
+    rightForceMat = forceMatrix(Force, TurnStarts, len(TurnStarts), stepLength)
+    meanRightForce = np.mean(rightForceMat, axis = 0)
+    sdRightForce = np.std(rightForceMat, axis = 0)
+    
+    result = EnsembleData(meanRightForce, sdRightForce)
+    
+    return(result)
 
 def findLandings(force, fThresh):
     """
@@ -61,13 +117,10 @@ def findLandings(force, fThresh):
     
     for step in range(len(force)-1):
     
-            
             if force[step] == 0 and force[step + 1] >= fThresh :
                 lic.append(step)
-    
-        
-    return lic
 
+    return lic
 
 
 def findTakeoffs(force, fThresh):
@@ -93,8 +146,6 @@ def findTakeoffs(force, fThresh):
         if force[step] >= fThresh and force[step + 1] == 0 :
             lto.append(step + 1)
     return lto 
-
-
 
 def delimitTrial(inputDF):
     """
@@ -125,6 +176,7 @@ def delimitTrial(inputDF):
     outputDat = inputDF.iloc[int(np.floor(pts[0,0])) : int(np.floor(pts[1,0])),:]
     outputDat = outputDat.reset_index()
     return(outputDat)
+
 
 def makeVizPlot(inputDF, inputLandings, inputTakeoffs):
     
@@ -194,176 +246,99 @@ def makeVizPlot(inputDF, inputLandings, inputTakeoffs):
     ax4.vlines(x = inputTakeoffs[1:5], ymin = 0, ymax = np.max(inputDF.RKneeMoment_Sagittal[inputLandings[0]:inputTakeoffs[3]]),
        color = 'cyan', label = 'Takeoffs',linewidth=3.0, ls='--')
     plt.show()
+
+def calcFullTrialStarts(fileName, filePath):
     
-    # fig3, ax = plt.subplots(1,1)
-    # ax.plot(dat.COM_Power)
-    # ax.set_ylabel('Power (W)')
-    # ax.set_title('COM Power')
-    # plt.show()
+    fName = fileName
+    fPath = filePath
     
-#makeVizPlot(dat, landings, takeoffs)
-
-CT = []
-
-impulseZ = []
-impulseX = []
-peakGRFz = []
-peakGRFx = []
-peakPFmom = []
-peakINVmom = []
-peakKneeEXTmom = []
-kneeABDrom = []
-eccWork = []
-peakPower = []
-
-impulse = []
-jumpTime = []
-
-
-subName = []
-config = []
-movements = []
-
-badFileList = []
-
-## save configuration names from files
-for fName in entries:
-    try:
-
-        config1 = fName.split('_')[1]
-        tmpMove = fName.split('_')[2]
-
+    config1 = fName.split('_')[1]
+    tmpMove = fName.split('_')[2]
+    
+    dat = pd.read_csv(fPath+fName,sep='\t', skiprows = 8, header = 0)
+    dat = dat.fillna(0)
+    
+    
+    landings = [] # erase landings and takeoffs from last loop
+    takeoffs = []
+    if (tmpMove == 'Skater') or (tmpMove == 'skater'):
         
-        dat = pd.read_csv(fPath+fName,sep='\t', skiprows = 8, header = 0)
-        dat = dat.fillna(0)
-        
-        
-        landings = [] # erase landings and takeoffs from last loop
-        takeoffs = []
-        if (tmpMove == 'Skater') or (tmpMove == 'skater'):
+        dat = delimitTrial(dat)
+        # create vector of force from vertical signal from each file and make low values 0
+        if np.max(abs(dat.FP3_GRF_Z)) > np.max(abs(dat.FP4_GRF_Z)):
+    
+            ZForce = dat.FP3_GRF_Z
+    
+            XForce = dat.FP3_GRF_X
             
-            dat = delimitTrial(dat)
-            # create vector of force from vertical signal from each file and make low values 0
-            if np.max(abs(dat.FP3_GRF_Z)) > np.max(abs(dat.FP4_GRF_Z)):
-
-                ZForce = dat.FP3_GRF_Z
-
-                XForce = dat.FP3_GRF_X
-                
-            else:
-                ZForce = dat.FP4_GRF_Z
-                XForce = dat.FP4_GRF_X 
-
-
-        
-                
-            if abs(np.min(XForce)) > abs(np.max(XForce)):
-                XForce = XForce * -1
-            
-            #dat = delimitTrialSkate(dat)
-            ZForce[ZForce<fThresh] = 0
-            
-            
-            #find the landings from function above
-            landings = findLandings(ZForce, fThresh)
-            takeoffs = findTakeoffs(ZForce, fThresh)
-            
-            landings[:] = [x for x in landings if x < takeoffs[-1]]
-            takeoffs[:] = [x for x in takeoffs if x > landings[0]]
-            
-         
-        elif (tmpMove == 'CMJ') or (tmpMove == 'cmj'):
-            
-            dat = delimitTrial(dat)
-            
-
-            ZForce = dat.FP2_GRF_Z
-            ZForce[ZForce<fThresh] = 0
-            
-            XForce = dat.FP2_GRF_X 
-          
-                    
-            
-            landings = findLandings(ZForce, fThresh)
-            takeoffs = findTakeoffs(ZForce, fThresh)
-            
-
-           
-            landings[:] = [x for x in landings if x < takeoffs[-1]]
-            takeoffs[:] = [x for x in takeoffs if x > landings[0]] 
-             
-            
-
-           
         else:
-            print('this movement is not included in Performance Test Analysis')
+            ZForce = dat.FP4_GRF_Z
+            XForce = dat.FP4_GRF_X 
+    
+        if abs(np.min(XForce)) > abs(np.max(XForce)):
+            XForce = XForce * -1
         
-        if (tmpMove == 'CMJ') or (tmpMove == 'cmj') or (tmpMove == 'Skater') or (tmpMove == 'skater'):
-            makeVizPlot(dat, landings, takeoffs)
-            answer = messagebox.askyesno("Question","Is data clean?")
-            
-            if answer == False:
-                plt.close('all')
-                print('Adding file to bad file list')
-                badFileList.append(fName)
-            
-            if answer == True:
-                plt.close('all')
-                print('Estimating point estimates')
-                
-                for i in range(len(landings)):
+        #dat = delimitTrialSkate(dat)
+        ZForce[ZForce<fThresh] = 0
         
-                    try:
-
-                        CT.append((takeoffs[i] - landings[i])/200)
-                        impulseZ.append(np.sum(ZForce[landings[i]:takeoffs[i]])/200)
-                        impulseX.append(np.sum(XForce[landings[i]:takeoffs[i]])/200)
-                        
-                        peakGRFz.append(np.max(ZForce[landings[i]:takeoffs[i]]))
-                        peakGRFx.append(np.max(XForce[landings[i]:takeoffs[i]]))
-                    
-                        peakPFmom.append(np.min(dat.RAnkleMoment_Sagittal[landings[i]:takeoffs[i]])*-1)
-                        peakINVmom.append(np.max(dat.RAnkleMoment_Frontal[landings[i]:takeoffs[i]]))
-                        peakKneeEXTmom.append(np.max(dat.RKneeMoment_Sagittal[landings[i]:takeoffs[i]]))
-                        kneeABDrom.append(np.max(dat.RKneeAngle_Frontal[landings[i]:takeoffs[i]]) - np.min(dat.RKneeAngle_Frontal[landings[i]:takeoffs[i]]))
-                        negpower = copy.deepcopy(dat.COM_Power)
-                        negpower[negpower>0] = 0
-                        eccWork.append(np.sum(negpower[landings[i]:takeoffs[i]])/200*-1)
-                        peakPower.append(np.max(dat.COM_Power[landings[i]:takeoffs[i]]))
-                        
         
-                        subName.append(fName.split('_')[0])
-                        config.append( config1 )
-                        movements.append( tmpMove )
-                        
+        #find the landings from function above
+        landings = findLandings(ZForce, fThresh)
+        takeoffs = findTakeoffs(ZForce, fThresh)
         
-                    except:
+        landings[:] = [x for x in landings if x < takeoffs[-1]]
+        takeoffs[:] = [x for x in takeoffs if x > landings[0]]
         
-                        print(fName + str(i))
+     
+    elif (tmpMove == 'CMJ') or (tmpMove == 'cmj'):
+        
+        dat = delimitTrial(dat)
+        
+        ZForce = dat.FP2_GRF_Z
+        ZForce[ZForce<fThresh] = 0
+        
+        XForce = dat.FP2_GRF_X 
+      
+        landings = findLandings(ZForce, fThresh)
+        takeoffs = findTakeoffs(ZForce, fThresh)
+      
+        landings[:] = [x for x in landings if x < takeoffs[-1]]
+        takeoffs[:] = [x for x in takeoffs if x > landings[0]] 
+         
+    else:
+        print('this movement is not included in Performance Test Analysis')
+    
+    if (tmpMove == 'CMJ') or (tmpMove == 'cmj') or (tmpMove == 'Skater') or (tmpMove == 'skater'):
+        makeVizPlot(dat, landings, takeoffs)
+        answer = messagebox.askyesno("Question","Is data clean?")
+        
+        if answer == False:
+            plt.close('all')
+            print('Adding file to bad file list')
+        
+        if answer == True:
+            plt.close('all')
+            print('Estimating point estimates')
 
-    except:
-        print(fName)
 
+    result = JumpStarts(tmpMove, config1, landings, takeoffs, dat)
+               
+    return(result)
 
+trial1 = calcFullTrialStarts(fName1, fPath)
+trial2 = calcFullTrialStarts(fName2, fPath)
 
+### calculating ensemble mean and SD ###
+x = np.linspace(0,stepLen,stepLen)
 
+## Calculating averaged data ## 
+## need to fix function to calculate right value for the given movement ##
+forcesTrial1 = calcEnsembleData(trial1.df.FP4_GRF_Z, trial1.landings, stepLen)
+forcesTrial2 = calcEnsembleData(trial2.df.FP4_GRF_Z, trial2.landings, stepLen)
 
-outcomes = pd.DataFrame({'Subject':list(subName), 'Config': list(config), 'Movement':list(movements),
-                         'CT':list(CT), 'impulse_Z':list(impulseZ), 'impulse_X':list(impulseX), 
-                         'peakGRF_Z':list(peakGRFz), 'peakGRF_X':list(peakGRFx), 'peakPFmom':list(peakPFmom),
-                         'peakINVmom':list(peakINVmom), 'peakKneeEXTmom':list(peakKneeEXTmom), 
-                         'kneeABDrom':list(kneeABDrom), 'eccWork':list(eccWork), 'peakPower':list(peakPower) })
-
-                       
-
-
-
-
-if save_on == 1:
-    outfileName = fPath + 'CompiledAgilityDataTest.csv'
-    outcomes.to_csv(outfileName, index = False)
-
-
-
-
-
+### plotting ###
+fig, (ax1, ax2) = plt.subplots(2)
+ax1.plot(x, forcesTrial1.meanData, 'k', color='#DC582A')
+ax1.fill_between(x,forcesTrial1.meanData-forcesTrial1.sdData, forcesTrial1.meanData+forcesTrial1.sdData,
+    alpha=0.5, edgecolor='#DC582A', facecolor='#DC582A', label = trial1.config)
+#ax1.plot(x, trial2.meanData, 'k', color='#00966C')
