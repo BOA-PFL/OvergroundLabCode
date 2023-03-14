7268b58b30000d0f4ab47192d18ef133efdee18f
Bethany Kilpatrick <85694373+Bethany-Kilpatrick@users.noreply.github.com>
2023-03-14 13:33:57 -0600

    Create PerformanceTest_Agility_TriangleAP.py

---
+++ Python/PerformanceTest_Agility_TriangleAP.py
@@ -0,0 +1,405 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  8 13:44:38 2023
+
+@author: Bethany.Kilpatrick
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 17 14:48:59 2021
+
+Updating 11/7/22 to include data visualization plots to 'pass' or fail
+and append the failing file names to a list while the passing ones write
+results to a csv - DF
+
+"""
+import sys
+sys.tracebacklimit = 0
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import copy
+from tkinter import messagebox
+
+pd.options.mode.chained_assignment = None  # default='warn' set to warn for a lot of warnings
+
+# Define constants and options
+fThresh = 80 #below this force value will be set to 0.
+save_on = 0 # turn this on for automatic saving of csv!!!! 
+#fPath = 'C:\\Users\\daniel.feeney\\Boa Technology Inc\\PFL Team - General\\Testing Segments\AgilityPerformanceData\\CPD_TongueLocatedDial_Oct2022\\Overground\\'
+
+fPath = 'C:\\Users\\bethany.kilpatrick\\Boa Technology Inc\\PFL - General\\Testing Segments\\AgilityPerformanceData\\2022_Tests\\CPD_TongueLocatedDial_Oct2022\\Overground\\'
+
+
+fileExt = r".txt"
+entries = [fName for fName in os.listdir(fPath) if fName.endswith(fileExt)]
+
+### set plot font size ###
+SMALL_SIZE = 14
+MEDIUM_SIZE = 16
+BIGGER_SIZE = 18
+
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+def findLandings(force, fThresh):
+    """
+    This function finds the landings from force plate data
+    it uses a heuristic to determine landings from when the smoothed force is
+    0 and then breaches a threshold
+    
+    Parameters
+    ----------
+    force : Pandas Series
+        Vertical force from force plate.
+    fThresh: integer
+        Value force has to be greater than to count as a takeoff/landing
+
+    Returns
+    -------
+    lic : list
+        Indices of landings.
+
+    """
+    lic = [] 
+    
+    for step in range(len(force)-1):
+    
+            
+            if force[step] == 0 and force[step + 1] >= fThresh :
+                lic.append(step)
+    
+        
+    return lic
+
+
+
+def findTakeoffs(force, fThresh):
+    """
+    This function calculates the takeoffs using a heuristic 
+
+    Parameters
+    ----------
+    force : Pandas Series
+        vertical force from force plate.
+    
+    fThresh: integer
+        Value force has to be greater than to count as a takeoff/landing
+    Returns
+    -------
+    lto : list
+        indices of takeoffs obtained from force data. Takeoffs here mean
+        the moment a force signal was > a threshold and then goes to 0
+
+    # """
+    lto = []
+    for step in range(len(force)-1):
+        if force[step] >= fThresh and force[step + 1] == 0 :
+            lto.append(step + 1)
+    return lto 
+
+
+
+def delimitTrial(inputDF,FName):
+    """
+     This function uses ginput to delimit the start and end of a trial
+    You will need to indicate on the plot when to start/end the trial. 
+    You must use tkinter or plotting outside the console to use this function
+    Parameters
+    ----------
+    inputDF : Pandas DataFrame
+        DF containing all desired output variables.
+    zForce : numpy array 
+        of force data from which we will subset the dataframe
+
+    Returns
+    -------
+    outputDat: dataframe subset to the beginning and end of jumps.
+
+    """
+
+    # generic function to plot and start/end trial #
+    if os.path.exists(fPath+FName+'TrialSeg.npy'):
+        trial_segment_old = np.load(fPath+FName+'TrialSeg.npy', allow_pickle =True)
+        trialStart = trial_segment_old[1][0,0]
+        trialEnd = trial_segment_old[1][1,0]
+        inputDF = inputDF.iloc[int(np.floor(trialStart)) : int(np.floor(trialEnd)),:]
+        outputDat = inputDF.reset_index()
+        
+    else: 
+        fig, ax = plt.subplots()
+
+        totForce = inputDF.FP1_GRF_Z + inputDF.FP2_GRF_Z + inputDF.FP3_GRF_Z + inputDF.FP4_GRF_Z
+        print('Select a point on the plot to represent the beginning & end of trial where Y-value is near 0')
+
+
+        ax.plot(totForce, label = 'Total Force')
+        fig.legend()
+        pts = np.asarray(plt.ginput(2, timeout=-1))
+        plt.close()
+        outputDat = inputDF.iloc[int(np.floor(pts[0,0])) : int(np.floor(pts[1,0])),:]
+        outputDat = outputDat.reset_index()
+        trial_segment = np.array([FName, pts], dtype = object)
+        np.save(fPath+FName+'TrialSeg.npy',trial_segment)
+
+    return(outputDat)
+
+
+def makeVizPlot(inputDF, inputLandings, inputTakeoffs):
+    
+    """
+    Parameters
+    ----------
+    inputDF : Pandas DF
+        Standard Agility export in dataframe.
+    inputLandings : list
+        list of landings from findLandings function.
+    inputTakeoffs : list
+        list of takeoffs from findTakeoffs function.
+
+    Returns
+    -------
+    No variables; just a plot to inspect for clean kinematic/kinetic data.
+
+    """
+    fig, ((ax, ax1), (ax2, ax3)) = plt.subplots(2, 2)
+    ax.plot(inputDF.RAnkleMoment_Sagittal[inputLandings[0]:inputTakeoffs[-1]], 'k')
+    # ax.vlines(x = inputLandings[1:], ymin = np.min(inputDF.RAnkleMoment_Sagittal[inputLandings[0]:inputTakeoffs[-1]]), ymax = np.max(inputDF.RAnkleMoment_Sagittal[inputLandings[0]:inputTakeoffs[-1]]),
+    #    color = 'coral', label = 'Landings',linewidth=3.0, ls='--')
+    # ax.vlines(x = inputTakeoffs[1:], ymin = np.min(inputDF.RAnkleMoment_Sagittal[inputLandings[0]:inputTakeoffs[-1]]), ymax = np.max(inputDF.RAnkleAngle_Sagittal[inputLandings[0]:inputTakeoffs[-1]]),
+    #    color = 'cyan', label = 'Takeoffs',linewidth=3.0, ls='--')
+    for i in range(len(inputLandings)):
+
+        ax.axvspan(inputLandings[i], inputTakeoffs[i], color = 'lightgray', alpha = 0.5)
+    
+    ax.set_ylim(-250, 10)
+    ax.set_xlabel('Indices')
+    ax.set_title('Ankle Sagittal Moment')
+    ax.set_ylabel('Moment (Nm)')
+    ax1.plot(inputDF.RKneeMoment_Sagittal[inputLandings[0]:inputTakeoffs[-1]], 'k')
+    for i in range(len(inputLandings)):
+
+        ax1.axvspan(inputLandings[i], inputTakeoffs[i], color = 'lightgray', alpha = 0.5)
+    ax1.set_ylim(-25, 250)
+    ax1.set_xlabel('Indices') 
+    ax1.set_title('Knee Sagittal Moment')
+    ax1.set_ylabel('Moment (Nm)')
+    plt.tight_layout()
+    plt.show()
+    
+
+    ax2.plot(inputDF.RAnkleMoment_Frontal[inputLandings[0]:inputTakeoffs[-1]], 'k')
+    for i in range(len(inputLandings)):
+
+        ax2.axvspan(inputLandings[i], inputTakeoffs[i], color = 'lightgray', alpha = 0.5)
+    ax2.set_ylim(-50, 150)
+    ax2.set_xlabel('Indices')
+    ax2.set_title('Ankle Frontal Moment')
+    ax2.set_ylabel('Moment (Nm)')
+    ax3.plot(inputDF.RKneeMoment_Frontal[inputLandings[0]:inputTakeoffs[-1]], 'k')
+    ax3.set_ylim(-100, 100)
+    ax3.set_xlabel('Indices')
+    ax3.set_title('Knee Frontal Moment')
+    ax3.set_ylabel('Moment (Nm)')
+    plt.tight_layout()
+    for i in range(len(inputLandings)):
+
+        ax3.axvspan(inputLandings[i], inputTakeoffs[i], color = 'lightgray', alpha = 0.5)
+    plt.show()
+    
+    # fig3, ax = plt.subplots(1,1)
+    # ax.plot(dat.COM_Power)
+    # ax.set_ylabel('Power (W)')
+    # ax.set_title('COM Power')
+    # plt.show()
+    
+#makeVizPlot(dat, landings, takeoffs)
+
+CT = []
+
+impulseZ = []
+impulseX = []
+peakGRFz = []
+peakGRFx = []
+# peakPFmom = []
+# peakINVmom = []
+# peakEVmom = []
+# peakKneeEXTmom = []
+# peakKneeADDmom = [] # Internal
+# kneeABDrom = []
+# eccWork = []
+# conWork = []
+# peakPower = []
+
+impulse = []
+jumpTime = []
+
+
+subName = []
+config = []
+movements = []
+
+badFileList = []
+
+## save configuration names from files
+for fName in entries:
+    try:
+        
+        # fName = entries[0]
+        config1 = fName.split('_')[1]
+        tmpMove = fName.split('_')[2]
+
+        
+        dat = pd.read_csv(fPath+fName,sep='\t', skiprows = 8, header = 0)
+        dat = dat.fillna(0)
+        
+        
+        landings = [] # erase landings and takeoffs from last loop
+        takeoffs = [] 
+        
+            
+            
+        if (tmpMove == 'Triangle') or (tmpMove == 'triangle'):
+            dat = delimitTrial(dat,fName)
+            ZForce = dat.FP2_GRF_Z
+            XForce = dat.FP2_GRF_X
+        # create vector of force from vertical signal from each file and make low values 0
+        # if np.max(abs(dat.FP2_GRF_Z)) > np.max(abs(dat.FP1_GRF_Z)):
+
+            
+        # else:
+        #     ZForce = dat.FP1_GRF_Z
+        #     XForce = dat.FP1_GRF_X 
+
+    
+        
+                
+            if abs(np.min(XForce)) > abs(np.max(XForce)):
+                XForce = XForce * -1
+            
+            
+            
+                ZForce[ZForce<fThresh] = 0
+            
+            
+            #find the landings from function above
+            landings = findLandings(ZForce, fThresh)
+            takeoffs = findTakeoffs(ZForce, fThresh)
+            
+            landings[:] = [x for x in landings if x < takeoffs[-1]]
+            takeoffs[:] = [x for x in takeoffs if x > landings[0]]
+            
+            
+        elif (tmpMove == 'AP') or (tmpMove == 'ap'):
+
+           dat = delimitTrial(dat,fName)
+
+ 
+
+           ZForce = dat.FP2_GRF_Z 
+           ZForce[ZForce<fThresh] = 0
+
+           XForce = dat.FP2_GRF_X
+           YForce = dat.FP2_GRF_Y
+
+
+           landings = findLandings(ZForce, fThresh )
+           takeoffs = findTakeoffs(ZForce, fThresh)
+
+ 
+
+    
+           landings[:] = [x for x in landings if x < takeoffs[-1]]
+           takeoffs[:] = [x for x in takeoffs if x > landings[0]] 
+            
+            
+
+        else:
+            print('this movement is not included in Performance Test Analysis')
+        
+        if (tmpMove == 'Triangle') or (tmpMove == 'triangle') or (tmpMove == 'AP') or (tmpMove == 'Triangle'):
+            makeVizPlot(dat, landings, takeoffs)
+            answer = messagebox.askyesno("Question","Is data clean?")
+            
+            if answer == False:
+                plt.close('all')
+                print('Adding file to bad file list')
+                badFileList.append(fName)
+            
+            if answer == True:
+                plt.close('all')
+                print('Estimating point estimates')
+                
+                for i in range(len(landings)):
+        
+                    try:
+                        
+                        tmpCT = round((takeoffs[i] - landings[i])/2) #to use as approximate for propulsive start
+                        CT.append((takeoffs[i] - landings[i])/200)
+                        impulseZ.append(np.sum(ZForce[landings[i]:takeoffs[i]])/200)
+                        impulseX.append(np.sum(XForce[landings[i]:takeoffs[i]])/200)
+                        
+                        peakGRFz.append(np.max(ZForce[landings[i]+tmpCT:takeoffs[i]])) #peak propulsive force approximated from 2nd half of contact time
+                        peakGRFx.append(np.max(XForce[landings[i]+tmpCT:takeoffs[i]])) #peak propulsive force approximated from 2nd half of contact time
+                    
+                        # peakPFmom.append(np.min(dat.RAnkleMoment_Sagittal[landings[i]:takeoffs[i]])*-1)
+                        # peakINVmom.append(np.max(dat.RAnkleMoment_Frontal[landings[i]:takeoffs[i]]))
+                        # peakEVmom.append(np.min(dat.RAnkleMoment_Frontal[landings[i]:takeoffs[i]]))
+                        # peakKneeADDmom.append(np.max(dat.RKneeMoment_Frontal[landings[i]:takeoffs[i]])) # looking at an INTERNAL moment, so this is the peak external ABD moment
+                        # peakKneeEXTmom.append(np.max(dat.RKneeMoment_Sagittal[landings[i]:takeoffs[i]]))
+                        # kneeABDrom.append(np.max(dat.RKneeAngle_Frontal[landings[i]:takeoffs[i]]) - np.min(dat.RKneeAngle_Frontal[landings[i]:takeoffs[i]]))
+                        # negpower = copy.deepcopy(dat.COM_Power)
+                        # negpower[negpower>0] = 0
+                        # eccWork.append(np.sum(negpower[landings[i]:takeoffs[i]])/200*-1)
+                        # pospower = copy.deepcopy(dat.COM_Power)
+                        # pospower[negpower<0] = 0
+                        # conWork.append(np.sum(pospower[landings[i]:takeoffs[i]])/200)
+                        # peakPower.append(np.max(dat.COM_Power[landings[i]:takeoffs[i]]))
+                        
+        
+                        subName.append(fName.split('_')[0])
+                        config.append( config1 )
+                        movements.append( tmpMove )
+                        
+        
+                    except:
+        
+                        print(fName + str(i))
+
+    except:
+        print(fName)
+
+
+
+
+
+outcomes = pd.DataFrame({'Subject':list(subName), 'Config': list(config), 'Movement':list(movements),
+                         'CT':list(CT), 'impulse_Z':list(impulseZ), 'impulse_X':list(impulseX), 
+                          'peakGRF_Z':list(peakGRFz), 'peakGRF_X':list(peakGRFx)}) 
+                          
+                          # 'peakPFmom':list(peakPFmom),
+                         # 'peakINVmom':list(peakINVmom), 'peakKneeEXTmom':list(peakKneeEXTmom), 
+                         # 'kneeABDrom':list(kneeABDrom), 'eccWork':list(eccWork), 'peakPower':list(peakPower) })
+
+
+
+                       
+
+
+
+save_on = 1
+if save_on == 1:
+    outfileName = fPath + 'CompiledAgilityDataTest.csv'
+    outcomes.to_csv(outfileName, index = False)
+
+
+
+
