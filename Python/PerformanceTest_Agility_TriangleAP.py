569fc8b909bc629150967e24430202fade3fd406
Bethany Kilpatrick <85694373+Bethany-Kilpatrick@users.noreply.github.com>
2023-08-16 13:28:58 -0600

    Update PerformanceTest_Agility_TriangleAP.py

--- Python/PerformanceTest_Agility_TriangleAP.py
+++ Python/PerformanceTest_Agility_TriangleAP.py
@@ -13,7 +13,12 @@ Updating 11/7/22 to include data visualization plots to 'pass' or fail
 and append the failing file names to a list while the passing ones write
 results to a csv - DF
 
-"""
+""" 
+
+
+
+
+
 import sys
 sys.tracebacklimit = 0
 
@@ -27,12 +32,14 @@ from tkinter import messagebox
 pd.options.mode.chained_assignment = None  # default='warn' set to warn for a lot of warnings
 
 # Define constants and options
-fThresh = 80 #below this force value will be set to 0.
-save_on = 0 # turn this on for automatic saving of csv!!!! 
-#fPath = 'C:\\Users\\daniel.feeney\\Boa Technology Inc\\PFL Team - General\\Testing Segments\AgilityPerformanceData\\CPD_TongueLocatedDial_Oct2022\\Overground\\'
+fThresh = 100 #below this force value will be set to 0.
+save_on = 1 # turn this on for automatic saving of csv!!!! 
 
-fPath = 'C:\\Users\\bethany.kilpatrick\\Boa Technology Inc\\PFL - General\\Testing Segments\\AgilityPerformanceData\\2022_Tests\\CPD_TongueLocatedDial_Oct2022\\Overground\\'
 
+fPath = 'C:\\Users\\bethany.kilpatrick\\Boa Technology Inc\\PFL - General\\Testing Segments\\AgilityPerformanceData\\AS_Trail_DorsalPressureVariation_PFLMech_May2023\\Overground\\Triangle&AP\\'
+#fPath = 'C:/Users/milena.singletary/Boa Technology Inc/PFL Team - Documents/General/Testing Segments/AgilityPerformanceData/AS_Trail_HeelLockAgility_Perf_Apr23/Overground/TriangleAP/'
+#fPath = 'C:\\Users\\milena.singletary\\Boa Technology Inc\\PFL Team - Documents\\General\\ExploratoryMetrics\\TriangleAP\\Agility_2023\\Exports\\'
+# fPath = 'C:\\Users\\milena.singletary\\Boa Technology Inc\\PFL Team - Documents\\General\\Testing Segments\\AgilityPerformanceData\\AS_Trail_MinimalStructure_CPDMech_June23\\Overground\\'
 
 fileExt = r".txt"
 entries = [fName for fName in os.listdir(fPath) if fName.endswith(fileExt)]
@@ -137,7 +144,7 @@ def delimitTrial(inputDF,FName):
     else: 
         fig, ax = plt.subplots()
 
-        totForce = inputDF.FP1_GRF_Z + inputDF.FP2_GRF_Z + inputDF.FP3_GRF_Z + inputDF.FP4_GRF_Z
+        totForce = inputDF.FP1_GRF_Z + inputDF.FP2_GRF_Z 
         print('Select a point on the plot to represent the beginning & end of trial where Y-value is near 0')
 
 
@@ -223,37 +230,35 @@ def makeVizPlot(inputDF, inputLandings, inputTakeoffs):
     
 #makeVizPlot(dat, landings, takeoffs)
 
-CT = []
 
+CT = []
 impulseZ = []
 impulseX = []
 peakGRFz = []
 peakGRFx = []
-# peakPFmom = []
-# peakINVmom = []
-# peakEVmom = []
-# peakKneeEXTmom = []
-# peakKneeADDmom = [] # Internal
-# kneeABDrom = []
-# eccWork = []
-# conWork = []
-# peakPower = []
+peakGRFy = []
+peakResForce = []
+peakGRFxy = []
+RFDecc_Z = []
+RFDecc_y = []
+pkPFmom = []
+pkKneeABDmom = []
 
-impulse = []
-jumpTime = []
 
+ZForce = []  
 
 subName = []
 config = []
 movements = []
 
 badFileList = []
+side = []
 
 ## save configuration names from files
 for fName in entries:
     try:
         
-        # fName = entries[0]
+        # fName = entries[1]
         config1 = fName.split('_')[1]
         tmpMove = fName.split('_')[2]
 
@@ -263,33 +268,30 @@ for fName in entries:
         
         
         landings = [] # erase landings and takeoffs from last loop
-        takeoffs = [] 
+        takeoffs = []  
+        
         
             
             
         if (tmpMove == 'Triangle') or (tmpMove == 'triangle'):
             dat = delimitTrial(dat,fName)
-            ZForce = dat.FP2_GRF_Z
-            XForce = dat.FP2_GRF_X
-        # create vector of force from vertical signal from each file and make low values 0
-        # if np.max(abs(dat.FP2_GRF_Z)) > np.max(abs(dat.FP1_GRF_Z)):
-
+            ZForce = dat.FP1_GRF_Z + dat.FP2_GRF_Z 
+            XForce = dat.FP1_GRF_X + (dat.FP2_GRF_X*-1)
+            YForce = dat.FP1_GRF_Y + dat.FP2_GRF_Y
+            resForce = []
+            XYForce = []
             
-        # else:
-        #     ZForce = dat.FP1_GRF_Z
-        #     XForce = dat.FP1_GRF_X 
-
-    
-        
-                
             if abs(np.min(XForce)) > abs(np.max(XForce)):
                 XForce = XForce * -1
             
             
+            ZForce[ZForce<fThresh] = 0
             
-                ZForce[ZForce<fThresh] = 0
-            
-            
+            # compute resultant GRF of X & Y
+            for ii in range(len(ZForce)):
+                XYForce.append(np.sqrt(YForce[ii]**2 + XForce[ii]**2))
+                resForce.append(np.sqrt(ZForce[ii]**2 + YForce[ii]**2 + XForce[ii]**2))
+                
             #find the landings from function above
             landings = findLandings(ZForce, fThresh)
             takeoffs = findTakeoffs(ZForce, fThresh)
@@ -297,82 +299,133 @@ for fName in entries:
             landings[:] = [x for x in landings if x < takeoffs[-1]]
             takeoffs[:] = [x for x in takeoffs if x > landings[0]]
             
+            for ii in landings:
+                if dat.FP2_GRF_Z[ii + 10] > fThresh:                
+                    xx = str(fName.split('_')[0]) + "- Right" + fName
+                    side.append(xx)
+                    # side.append('Right',fName.split('_')[0])  
+                    # side.append(fName.split('_')[0])
+                    
+                if dat.FP1_GRF_Z[ii+ 10] > fThresh:
+                    xx = str(fName.split('_')[0]) + "- Left" + fName 
+                    side.append(xx)
+                    # side.append('Left',fName.split('_')[0] )   
+                    # side.append(fName.split('_')[0])
+                    # XForce = XForce * -1
+            
+            
             
         elif (tmpMove == 'AP') or (tmpMove == 'ap'):
-
+   
+            # for ii in landings: 
+            #     side.append('NA')
            dat = delimitTrial(dat,fName)
+           ZForce = dat.FP1_GRF_Z + dat.FP2_GRF_Z 
+           XForce = dat.FP1_GRF_X + (dat.FP2_GRF_X * -1) # to account for direction (L v R)
+           YForce = dat.FP1_GRF_Y + dat.FP2_GRF_Y
+           resForce = []
+           XYForce = []
+               
+           if abs(np.min(XForce)) > abs(np.max(XForce)):
+                XForce = XForce * -1
 
- 
-
-           ZForce = dat.FP2_GRF_Z 
            ZForce[ZForce<fThresh] = 0
-
-           XForce = dat.FP2_GRF_X
-           YForce = dat.FP2_GRF_Y
-
-
-           landings = findLandings(ZForce, fThresh )
+           
+           # compute resultant force    
+           for ii in range(len(ZForce)):
+               XYForce.append(np.sqrt(YForce[ii]**2 + XForce[ii]**2))
+               resForce.append(np.sqrt(ZForce[ii]**2 + YForce[ii]**2 + XForce[ii]**2))
+               
+           # find the landings from function above
+           landings = findLandings(ZForce, fThresh)
            takeoffs = findTakeoffs(ZForce, fThresh)
-
- 
-
-    
+           
            landings[:] = [x for x in landings if x < takeoffs[-1]]
-           takeoffs[:] = [x for x in takeoffs if x > landings[0]] 
-            
+           takeoffs[:] = [x for x in takeoffs if x > landings[0]]
+           
+           for ii in landings:
+               if dat.FP2_GRF_Z[ii + 10] > fThresh:                
+                   xx = str(fName.split('_')[0]) + "- Right" + fName
+                   side.append(xx)
+                   # side.append('Right') 
+                   # side.append(fName.split('_')[0])
+               if dat.FP1_GRF_Z[ii+ 10] > fThresh:
+                   xx = str(fName.split('_')[0]) + "- Left" + fName
+                   side.append(xx)
+                   # side.append('Left', fName.split('_')[0]) 
+                   # side.append(fName.split('_')[0])
+ 
             
 
         else:
             print('this movement is not included in Performance Test Analysis')
         
-        if (tmpMove == 'Triangle') or (tmpMove == 'triangle') or (tmpMove == 'AP') or (tmpMove == 'Triangle'):
+        if (tmpMove == 'Triangle') or (tmpMove == 'triangle') or (tmpMove == 'AP') or (tmpMove == 'ap'):
             makeVizPlot(dat, landings, takeoffs)
-            answer = messagebox.askyesno("Question","Is data clean?")
+            # answer = messagebox.askyesno("Question","Is data clean?")
             
-            if answer == False:
-                plt.close('all')
-                print('Adding file to bad file list')
-                badFileList.append(fName)
+            # if answer == False:
+            #     plt.close('all')
+            #     print('Adding file to bad file list')
+            #     badFileList.append(fName)
             
-            if answer == True:
-                plt.close('all')
-                print('Estimating point estimates')
+            # if answer == True:
+            #     plt.close('all')
+            #     print('Estimating point estimates')
                 
-                for i in range(len(landings)):
-        
-                    try:
-                        
-                        tmpCT = round((takeoffs[i] - landings[i])/2) #to use as approximate for propulsive start
-                        CT.append((takeoffs[i] - landings[i])/200)
-                        impulseZ.append(np.sum(ZForce[landings[i]:takeoffs[i]])/200)
-                        impulseX.append(np.sum(XForce[landings[i]:takeoffs[i]])/200)
-                        
-                        peakGRFz.append(np.max(ZForce[landings[i]+tmpCT:takeoffs[i]])) #peak propulsive force approximated from 2nd half of contact time
-                        peakGRFx.append(np.max(XForce[landings[i]+tmpCT:takeoffs[i]])) #peak propulsive force approximated from 2nd half of contact time
+               
+            for i in range(len(landings)):
+                
+                
+                try: 
+                    subName.append(fName.split('_')[0])
+                    config.append( config1 )
+                    movements.append( tmpMove )
                     
-                        # peakPFmom.append(np.min(dat.RAnkleMoment_Sagittal[landings[i]:takeoffs[i]])*-1)
-                        # peakINVmom.append(np.max(dat.RAnkleMoment_Frontal[landings[i]:takeoffs[i]]))
-                        # peakEVmom.append(np.min(dat.RAnkleMoment_Frontal[landings[i]:takeoffs[i]]))
-                        # peakKneeADDmom.append(np.max(dat.RKneeMoment_Frontal[landings[i]:takeoffs[i]])) # looking at an INTERNAL moment, so this is the peak external ABD moment
-                        # peakKneeEXTmom.append(np.max(dat.RKneeMoment_Sagittal[landings[i]:takeoffs[i]]))
-                        # kneeABDrom.append(np.max(dat.RKneeAngle_Frontal[landings[i]:takeoffs[i]]) - np.min(dat.RKneeAngle_Frontal[landings[i]:takeoffs[i]]))
-                        # negpower = copy.deepcopy(dat.COM_Power)
-                        # negpower[negpower>0] = 0
-                        # eccWork.append(np.sum(negpower[landings[i]:takeoffs[i]])/200*-1)
-                        # pospower = copy.deepcopy(dat.COM_Power)
-                        # pospower[negpower<0] = 0
-                        # conWork.append(np.sum(pospower[landings[i]:takeoffs[i]])/200)
-                        # peakPower.append(np.max(dat.COM_Power[landings[i]:takeoffs[i]]))
+                    tmpCT = round((takeoffs[i] - landings[i])/2) #to use as approximate for propulsive start
+                    
+                    CT.append((takeoffs[i] - landings[i])/200)
+                    impulseZ.append(np.sum(ZForce[landings[i]:takeoffs[i]])/200)
+                    impulseX.append(np.sum(XForce[landings[i]:takeoffs[i]])/200)
+                    
+                    peakGRFz.append(np.max(ZForce[landings[i]+tmpCT:takeoffs[i]])) #peak propulsive force approximated from 2nd half of contact time
+                    peakGRFx.append(np.max(XForce[landings[i]+tmpCT:takeoffs[i]])) #peak propulsive force approximated from 2nd half of contact time
+                    peakGRFy.append(np.max(YForce[landings[i]+tmpCT:takeoffs[i]]))
+                    peakResForce.append(np.max(resForce[landings[i]+tmpCT:takeoffs[i]]))
+                    peakGRFxy.append(np.max(XYForce[landings[i]+tmpCT:takeoffs[i]]))
+                    
+                    RFDecc_Z.append(np.max(np.diff(ZForce[landings[i]:landings[i]+tmpCT]))/200) # braking RFD, from intitial contact upto 1/2 CT ~ propulsive start
+                    RFDecc_y.append(np.max(np.diff(YForce[landings[i]:landings[i]+tmpCT]))/200)
+                    
+                    # if (side[i] == 'Left'):
+                    #     ShankYMin = np.argmin(dat.L_ShankPos_Y[landings[i]: takeoffs[i]])
+                    #     # LPkAnkPlantMom.append( np.max( dat.LAnkleMoment_Sagittal[landings[i]+tmpCT: takeoffs[i]]))  
+                    #     # LPkKneeABDMom.append( np.min( dat.LKneeMoment_Frontal[landings[i]+tmpCT: takeoffs[i]]))   
+                    #     # pkPFmom.append(np.min( dat.LAnkleMoment_Sagittal[landings[i]: takeoffs[i]])*-1)
+                    #     # pkKneeABDmom.append(np.min( dat.LKneeMoment_Frontal[landings[i]: takeoffs[i]]))
                         
-        
-                        subName.append(fName.split('_')[0])
-                        config.append( config1 )
-                        movements.append( tmpMove )
+                    # if (side[i] == 'Right'):
+                    #     ShankYMin = np.argmin(dat.R_ShankPos_Y[landings[i]: takeoffs[i]])
+                    #     # RPkAnkPlantMom.append( np.max( dat.RAnkleMoment_Sagittal[landings[i]+tmpCT: takeoffs[i]]))  
+                    #     # RPkKneeABDMom.append( np.min( dat.RKneeMoment_Frontal[landings[i]+tmpCT: takeoffs[i]]))                                                           
+                    #     # pkPFmom.append(np.min( dat.RAnkleMoment_Sagittal[landings[i]: takeoffs[i]])*-1)  
+                    #     # pkKneeABDmom.append(np.min( dat.RKneeMoment_Frontal[landings[i]: takeoffs[i]]))
                         
-        
-                    except:
-        
-                        print(fName + str(i))
+    
+                    
+                    
+                    # # for RFD_y to be computed ShankPosY needs to be exported as TMM variable 
+                    # if ShankYMin == 0:
+                    #         RFDecc_y.append(np.max(np.diff(YForce[landings[i]:landings[i]+tmpCT]))/200)
+                    # else:
+                    #         RFDecc_y.append(np.max(np.diff(YForce[landings[i]: landings[i] + ShankYMin]))/200)
+               
+                    
+                except:
+    
+                    print(fName + str(i))
+
+
 
     except:
         print(fName)
@@ -381,23 +434,19 @@ for fName in entries:
 
 
 
-outcomes = pd.DataFrame({'Subject':list(subName), 'Config': list(config), 'Movement':list(movements),
+outcomes = pd.DataFrame({'Subject':list(subName), 'Config': list(config), 'Movement':list(movements), 
                          'CT':list(CT), 'impulse_Z':list(impulseZ), 'impulse_X':list(impulseX), 
-                          'peakGRF_Z':list(peakGRFz), 'peakGRF_X':list(peakGRFx)}) 
+                          'peakGRF_Z':list(peakGRFz), 'peakGRF_X':list(peakGRFx),'peakGRF_Y':list(peakGRFy), 'peakGRF_R':list(peakResForce), 'peakGRF_XY':list(peakGRFxy),
+                          'brakingRFDvertical': list(RFDecc_Z),   'breakingRFDy':list(RFDecc_y) }) 
+                          # 'pkPFmom' :list(pkPFmom), 'pkKneeABDmom':list(pkKneeABDmom) ,'Side':list(side),  }) 
                           
-                          # 'peakPFmom':list(peakPFmom),
-                         # 'peakINVmom':list(peakINVmom), 'peakKneeEXTmom':list(peakKneeEXTmom), 
-                         # 'kneeABDrom':list(kneeABDrom), 'eccWork':list(eccWork), 'peakPower':list(peakPower) })
+                     
 
 
-
-                       
-
-
-
-save_on = 1
+# save_on = 1
 if save_on == 1:
-    outfileName = fPath + 'CompiledAgilityDataTest.csv'
+    outfileName = 'C:\\Users\\bethany.kilpatrick\\Boa Technology Inc\\PFL - General\\Testing Segments\\AgilityPerformanceData\\AS_Trail_DorsalPressureVariation_PFLMech_May2023\\Overground\\Triangle&AP\\CompiledAgilityDataTest_TriangleAP_Kinetics_2.csv'
+    # outfileName = 'C:\\Users\\milena.singletary\\Boa Technology Inc\\PFL Team - Documents\General\\Testing Segments\\AgilityPerformanceData\\AS_Trail_HeelLockAgility_Perf_Apr23\\Overground\\TriangleAP\\CompiledAgilityDataTest_TriangleAP_Kinetics_3.csv'
     outcomes.to_csv(outfileName, index = False)
 
 
