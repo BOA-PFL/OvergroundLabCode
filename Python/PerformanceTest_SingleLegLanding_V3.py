1f7c233efbd7df4b183bf42f40932a88cf4d49fc
milenasingletary <105753587+milenasingletary@users.noreply.github.com>
2024-08-23 16:40:08 -0600

    Update PerformanceTest_SingleLegLanding_V3.py
    adding order to export

--- Python/PerformanceTest_SingleLegLanding_V3.py
+++ Python/PerformanceTest_SingleLegLanding_V3.py
@@ -249,6 +249,7 @@ sdFz = []
 subName = []
 movements = []
 tmpConfig = []
+order = []
 badFileList = []
 
 RankleEvMom = []
@@ -271,6 +272,7 @@ for fName in entries:
         ##Parse file name into subject and configuration 
         config = fName.split(sep = "_")[1]
         tmpMove = fName.split(sep = "_")[2] 
+        forder = fName.split(sep = "_")[3][0]
         # Filter force
         forceZ = np.array(dat.FP4_GRF_Z)
         forceZ[forceZ<fThresh] = 0 
@@ -362,7 +364,7 @@ for fName in entries:
 
 
 
-outcomes = pd.DataFrame({'Subject':list(subName), 'Config': list(tmpConfig),'Movement':list(movements), 'StabTime': list(stabilization), 'sdFz':list(sdFz),
+outcomes = pd.DataFrame({'Subject':list(subName), 'Config': list(tmpConfig), 'Order': list(order), 'Movement':list(movements), 'StabTime': list(stabilization), 'sdFz':list(sdFz),
                           'RankleEvMom':list(RankleEvMom), 'RankleInMom':list(RankleInMom),  'RkneeADDMom': list(RkneeADDMom), 
                           'RkneeABDMom':list(RkneeABDMom)})
 
